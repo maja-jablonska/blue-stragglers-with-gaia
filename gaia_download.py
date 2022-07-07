6a66ab95b9bd265d6385517d8ccb2dc2611ef904
Maja Jablonska <mjablons@hqd016557.ads.eso.org>
2022-07-07 09:55:09 +0200

    Add logging to the download

--- gaia_download.py
+++ gaia_download.py
@@ -8,6 +8,13 @@ import astropy.units as u
 Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 Gaia.ROW_LIMIT = -1 # Fetch all results
 
+
+def format_coord(coord: float) -> str:
+    two_dec: str = '{0:.2f}'.format(coord)
+    with_sign = '+'+two_dec if coord>0 else two_dec
+    return with_sign
+
+
 def get_sources_by_random_index(rows: int = 1000, start_index: int = 0) -> pd.DataFrame:
     job = Gaia.launch_job(f'''
         SELECT source_id, ra, dec, parallax, parallax_error, 
@@ -30,17 +37,28 @@ def gaia_cone_search_5d(ra: float,
         :param radius: radius of the area we wish to search in [degrees]
         :return table of 5D data for objects in the area
     """
-    job = Gaia.launch_job_async(f'''
+    
+    ra_str: str = format_coord(ra)
+    dec_str: str = format_coord(dec)
+    
+    print(f"Executing cone search for ra={ra_str} and dec={dec_str} with radius of {radius}...")
+    
+    query: str = f'''
         SELECT source_id, ra, dec, parallax, parallax_error, 
         pmra, pmra_error, pmdec, pmdec_error, DISTANCE(
-           POINT('ICRS', +{str(ra)}, +{str(dec)}),
+           POINT('ICRS', {ra_str}, {dec_str}),
            POINT('ICRS', ra, dec)) AS ang_sep  
         FROM gaiadr3.gaia_source 
         WHERE 1 = CONTAINS( 
             POINT('ICRS', ra, dec), 
-            CIRCLE('ICRS', +{str(ra)}, +{str(dec)}, 1.0)) 
+            CIRCLE('ICRS', {ra_str}, {dec_str}, {radius})) 
         AND parallax > {min_parallax} AND parallax < {max_parallax} 
-    ''', output_format='csv')
+    '''
+        
+    print('Executing query:')
+    print(query)
+    
+    job = Gaia.launch_job_async(query, output_format='csv')
 
     print(f"Query finished!")
     
